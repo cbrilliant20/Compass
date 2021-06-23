e75e4711e2a8cf8495676a0b17c3685b82301d7c
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-06-23 06:18:45 -0400

    rmove boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   
-
   resources :itineraries
   resources :trips
   resources :users, only: :create
