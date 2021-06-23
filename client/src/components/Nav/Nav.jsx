44f45ac8e45c8716a8596d0201003d5fced1af23
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-06-23 10:16:09 -0400

    nav wip

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,34 @@
 import "./Nav.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faDraftingCompass } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 const Nav = (props) => {
+  const [showMenu, setShowMenu] = useState(false)
+  
+  if (showMenu) {
+    return className="nav-links"
+  }
 
-  return(
-<h1>Nav</h1>
+
+  return (
+    <nav>
+      <div className="logo">
+        <FontAwesomeIcon className="logo-icon" icon={faDraftingCompass} />
+        <h1>COMPASS</h1>
+      </div>
+      <div className="burger">
+        <FontAwesomeIcon
+          className="svg-inline--fa fa-bars fa-w-20 burger-icon"
+          icon={faBars}
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        <div className="nav-links">
+          <h1>Hi</h1>
+        </div>
+      </div>
+    </nav>
   )
 }
 
